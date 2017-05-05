87ed2eac7d8d1b72f7bf33d5c51872c6621ec82e
chbs8 <christian.bs.8@gmail.com>
2017-05-05 10:29:46 +0200

    Initial commit

---
+++ Lab2_ESDC/Lab2_ESDC.sdk/echo_demo/Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+LD_SRCS += \
+../src/lscript.ld 
+
+C_SRCS += \
+../src/dispatch.c \
+../src/echo.c \
+../src/main.c 
+
+OBJS += \
+./src/dispatch.o \
+./src/echo.o \
+./src/main.o 
+
+C_DEPS += \
+./src/dispatch.d \
+./src/echo.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM gcc compiler'
+	arm-xilinx-eabi-gcc -Wall -O0 -g3 -c -fmessage-length=0 -I../../echo_demo_bsp/ps7_cortexa9_0/include -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ socket_client/Debug/src/txperf.d
@@ -0,0 +1,64 @@
+src/txperf.d: ../src/txperf.c \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/lwip/inet.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/lwip/opt.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/lwipopts.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/lwip/debug.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/lwip/arch.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/arch/cc.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/lwip/def.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/lwip/ip_addr.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/lwip/sockets.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/FreeRTOS.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/projdefs.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/portable.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/portmacro.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/mpu_wrappers.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/task.h \
+ ../../socket_client_bsp/ps7_cortexa9_0/include/list.h
+
+../../socket_client_bsp/ps7_cortexa9_0/include/lwip/inet.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/lwip/opt.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/lwipopts.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/lwip/debug.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/lwip/arch.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/arch/cc.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/lwip/def.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/lwip/ip_addr.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/lwip/sockets.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/FreeRTOS.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/projdefs.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/FreeRTOSConfig.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/portable.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/portmacro.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/mpu_wrappers.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/task.h:
+
+../../socket_client_bsp/ps7_cortexa9_0/include/list.h:
